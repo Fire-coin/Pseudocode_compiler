3fa4376a441c005d0a3b4581eb53b3ebc61adbf0
Fire Coin <maxim.m.kollar@gmail.com>
2025-05-18 15:50:06 +0200

    Added keywords.txt again. Created join, loadKeywords and part of handleDeclare functions. Switching keywords from static array into unordered_map.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,29 @@
 #include <sstream>
 #include <vector>
 #include <utility> // for std::pair
+#include <unordered_map>
 
+
+bool loadKeywords(std::string filename);
 
 std::vector<std::string> split(std::string line, char delimiter);
+bool handleDeclare(std::string line);
+std::string join(std::vector<std::string> args);
+
 std::pair<std::string, std::string> dataTypes[5] = {{"INTEGER", "int"}, {"REAL", "double"},
                                                     {"CHAR", "char"}, {"STRING", "string"},
                                                     {"BOOLEAN", "bool"}};
 
+std::unordered_map<std::string, int> keywords = {{"DECLARE", 0}};
+
 int main() {
+    // Loading kaywords from a file
+    bool success = loadKeywords("keywords.txt");
+    if (!success) {
+        std::cerr << "Could not load keywords.\n";
+        return 0;
+    }
+
     std::string inputFile;
     std::string outputFile = "output.cpp";
     std::cout << "Enter filename: ";
@@ -30,11 +45,28 @@ int main() {
         for (auto i : args) {
             std::cout << i << ',';
         }
+        // std::cout << join(args) << '\n';
         std::cout << '\n';
     }
 
     return 0;
 }
+
+
+bool loadKeywords(std::string filename) {
+    std::ifstream fin(filename);
+    if (!fin.is_open()) {
+        return false;
+    }
+    std::string buffer;
+    std::vector<std::string> pairs;
+    while (std::getline(fin, buffer, '\n')) {
+        pairs = split(buffer, ':');
+        keywords[pairs[0]] = stoi(pairs[1]);
+    }
+    return true;
+}
+
 
 std::vector<std::string> split(std::string line, char delimiter) {
     std::vector<std::string> output;
@@ -45,4 +77,25 @@ std::vector<std::string> split(std::string line, char delimiter) {
     }
 
     return output;
+}
+
+
+std::string join(std::vector<std::string> args) {
+    std::string output;
+    for (std::string s : args) {
+        output += s;
+    }
+    return output;
+}
+
+
+// This function supports only single declaration on each line.
+bool handleDeclare(std::string line) {
+    std::vector<std::string> args = split(line, ':');
+    if (args.size() != 2) {
+        throw std::invalid_argument("There should be 2 argumenst for declaration; <identifier> : <data_type>");
+    }
+    
+
+    return true;
 }
